9315e5de6d890ba316c26c134c06c5d8b6284b17
Caleb Cornett <Caleb.cornett@outlook.com>
2024-09-12 18:02:59 -0500

    VertexBinding -> VertexBufferDescription (#40)

--- Examples/BasicVertexBuffer.c
+++ Examples/BasicVertexBuffer.c
@@ -36,21 +36,21 @@ static int Init(Context* context)
 		},
 		// This is set up to match the vertex shader layout!
 		.vertex_input_state = (SDL_GPUVertexInputState){
-			.num_vertex_bindings = 1,
-			.vertex_bindings = (SDL_GPUVertexBinding[]){{
-				.index = 0,
+			.num_vertex_buffers = 1,
+			.vertex_buffer_descriptions = (SDL_GPUVertexBufferDescription[]){{
+				.slot = 0,
 				.input_rate = SDL_GPU_VERTEXINPUTRATE_VERTEX,
 				.instance_step_rate = 0,
 				.pitch = sizeof(PositionColorVertex)
 			}},
 			.num_vertex_attributes = 2,
 			.vertex_attributes = (SDL_GPUVertexAttribute[]){{
-				.binding_index = 0,
+				.buffer_slot = 0,
 				.format = SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
 				.location = 0,
 				.offset = 0
 			}, {
-				.binding_index = 0,
+				.buffer_slot = 0,
 				.format = SDL_GPU_VERTEXELEMENTFORMAT_UBYTE4_NORM,
 				.location = 1,
 				.offset = sizeof(float) * 3
