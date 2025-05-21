fac458a8c89efc69ac4df52c8c4ad548c9cee354
o:tone <33697887+itsdanott@users.noreply.github.com>
2025-05-21 19:41:03 +0200

    Add shadercross compile.bat

---
+++ Content/Shaders/Source/compile.bat
@@ -0,0 +1,27 @@
+rem Requires shadercross CLI installed from SDL_shadercross
+@echo off
+setlocal enabledelayedexpansion
+
+for %%f in (*.vert.hlsl) do (
+    if exist "%%f" (
+        shadercross "%%f" -o "..\Compiled\SPIRV\%%~nf.spv"
+        shadercross "%%f" -o "..\Compiled\MSL\%%~nf.msl"
+        shadercross "%%f" -o "..\Compiled\DXIL\%%~nf.dxil"
+    )
+)
+
+for %%f in (*.frag.hlsl) do (
+    if exist "%%f" (
+        shadercross "%%f" -o "..\Compiled\SPIRV\%%~nf.spv"
+        shadercross "%%f" -o "..\Compiled\MSL\%%~nf.msl"
+        shadercross "%%f" -o "..\Compiled\DXIL\%%~nf.dxil"
+    )
+)
+
+for %%f in (*.comp.hlsl) do (
+    if exist "%%f" (
+        shadercross "%%f" -o "..\Compiled\SPIRV\%%~nf.spv"
+        shadercross "%%f" -o "..\Compiled\MSL\%%~nf.msl"
+        shadercross "%%f" -o "..\Compiled\DXIL\%%~nf.dxil"
+    )
+)
